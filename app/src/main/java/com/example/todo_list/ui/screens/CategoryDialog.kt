40ceb76f2abd70efee69f67f1bbb99ec69878731
Jakub <245762@edu.p.lodz.pl>
2025-06-06 12:33:47 +0200

    Now Category Color saves properly!

--- app/src/main/java/com/example/todo_list/ui/screens/CategoryDialog.kt
+++ app/src/main/java/com/example/todo_list/ui/screens/CategoryDialog.kt
@@ -1,6 +1,7 @@
 package com.example.todo_list.ui.screens
 
 import TopBarPopUp
+import android.util.Log
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
@@ -28,6 +29,7 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -111,7 +113,7 @@ fun CategoryDialog(dismiss: () -> Unit, viewModel: TaskViewModel) {
                                 viewModel.addCategory(
                                     Category(
                                         title = name,
-                                        colorLong = selectedColor.value.toLong()
+                                        colorLong = selectedColor.toArgb().toLong()
                                     )
                                 )
                                 dismiss()
